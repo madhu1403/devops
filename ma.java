6e01385e285b3ba83ccba35b6890f8f80667a532
madhu1403 <sharadajaganjsm@gmail.com>
2024-06-09 20:34:16 +0530

    first

--- ma.java
+++
@@ -1,5 +0,0 @@
-class ma{
-  public static void main(String[] args){
-     System.out.println("hello world");
-}
-}
--- ma1.java
+++
@@ -1,5 +0,0 @@
-class ma1{
-  public static void main(String[] args){
-     System.out.println("hello world");
-}
-}
